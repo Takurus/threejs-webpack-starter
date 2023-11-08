28e2a1cc7286702e7c0958aa9fa80a8bcc464f94
Goran Blixt <goranblixt@Gorans-MacBook-Pro.local>
2023-11-08 05:13:34 +0100

    main

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Loading
+const textureLoader = new THREE.TextureLoader()
+
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,12 +18,16 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.SphereBufferGeometry( .5, 64, 64 ); 
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.normalMap = normalTexture;
+
+material.color = new THREE.Color(0x292929)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -31,6 +40,20 @@ pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xff0000, 0.2)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+pointLight2.position.set(1,1,1)
+pointLight2.intensity = 1
+
+scene.add(pointLight2)
+
+gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
+gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+gui.add(pointLight2, 'intensity').min(-0).max(10).step(0.01)
 
 /**
  * Sizes
@@ -73,10 +96,12 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(0x404040, 1) // Set background color to black
 
 /**
  * Animate
@@ -103,3 +128,43 @@ const tick = () =>
 }
 
 tick()
+
+// //-------------------------------------------logo
+
+// // Create a scene2
+// const scene2 = new THREE.Scene();
+
+// // Create a camera
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// camera.position.z = 5;
+
+// // Create a renderer2
+// const renderer2 = new THREE.WebGLRenderer({ antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// // Load the logo texture
+// const loader = new THREE.TextureLoader();
+// loader.load(
+//   'path/to/your/logo.jpg',
+//   function (texture) {
+//     // Create a material using the logo texture
+//     const material = new THREE.MeshBasicMaterial({ map: texture });
+
+//     // Create a geometry for the logo
+//     const geometry = new THREE.PlaneGeometry(2, 2);
+
+//     // Create a mesh using the geometry and material
+//     const mesh = new THREE.Mesh(geometry, material);
+
+//     // Add the mesh to the scene
+//     scene.add(mesh);
+
+//     // Render the scene with the camera
+//     renderer.render(scene, camera);
+//   },
+//   undefined,
+//   function (error) {
+//     console.error('An error happened.', error);
+//   }
+// );
